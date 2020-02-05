fc78eca1fa7eeb67faed5860c7b97d084a5a7cdf
Artur Borkowski <arturborkowski7@gmail.com>
2020-02-05 23:58:50 +0100

    add forum

--- kodilla-testing/src/main/java/com/kodilla/testing/forum/ForumPost.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/ForumPost.java
@@ -33,7 +33,7 @@ public class ForumPost {
     @Override
     public int hashCode() {
         int result = postBody.hashCode();
-        result = 31 * result + author.hashCode() ;
+        result = 31 * result + author.hashCode();
         return result;
     }
 }
